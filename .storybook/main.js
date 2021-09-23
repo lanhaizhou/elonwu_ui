ca8293a1552e9066b7cbee4ae1092fd5366e28d3
吴渊 <wrzmonkey@gmail.com>
2021-09-23 12:06:01 +0800

    fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  stories: [
-    '../packages/**/*.stories.mdx',
-    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../web/**/*.stories.mdx', '../web/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
