1e72e066ee27674366d8089d9d9f69411745cd09
lanhaizhou <lanhaizhouzhou@foxmail.com>
2021-10-15 11:26:47 +0800

    update Toggle

---
+++ packages/web/toggle/src/assets/index.ts
@@ -0,0 +1,11 @@
+import ButtonSelectSvg from './button-select.svg';
+import ButtonUnSelectSvg from './button-unselect.svg';
+import checkboxSelectSvg from './checkbox-select.svg';
+import checkboxUnSelectSvg from './checkbox-unselect.svg';
+
+export {
+  ButtonSelectSvg,
+  ButtonUnSelectSvg,
+  checkboxSelectSvg,
+  checkboxUnSelectSvg,
+};
